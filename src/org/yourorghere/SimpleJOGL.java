8ebb2c7928b9c2422f9fc138ef2bafaf63ad7dca
student <student@240-08.INFORMATYKA.local>
2016-10-06 11:17:35 +0200

    zad 2 - kolory

--- src/org/yourorghere/SimpleJOGL.java
+++ src/org/yourorghere/SimpleJOGL.java
@@ -94,6 +94,7 @@ public class SimpleJOGL implements GLEventListener {
         gl.glTranslatef(-1.5f, 0.0f, -6.0f);
         
         gl.glBegin(GL.GL_TRIANGLES);
+            gl.glColor3f(1.0f,0.0f,0.0f);
             gl.glVertex3f(-1.0f,1.0f, -6.0f);
             gl.glVertex3f(-2.0f, -1.0f, -6.0f);
             gl.glVertex3f(0.0f, -1.0f, -6.0f);
@@ -102,11 +103,13 @@ public class SimpleJOGL implements GLEventListener {
         gl.glTranslatef(3.0f, 0.0f, 0.0f);
         
          gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(0.0f,8.0f,7.0f);
             gl.glVertex3f(-1.0f, 1.0f, 0.0f);  // Top Left
             gl.glVertex3f(1.0f, 1.0f, 0.0f);   // Top Right
             gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
             gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
         gl.glEnd();
+       
          
             
 
