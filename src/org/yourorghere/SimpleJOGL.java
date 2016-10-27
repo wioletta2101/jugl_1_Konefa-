7f86c8e5f83612b1e252ae9e3de530df046f459d
student <student@240-06.INFORMATYKA.local>
2016-10-27 11:17:49 +0200

    oswietlenie

--- src/org/yourorghere/SimpleJOGL.java
+++ src/org/yourorghere/SimpleJOGL.java
@@ -15,224 +15,303 @@ import javax.media.opengl.glu.GLU;
 
 
 /**
- * SimpleJOGL.java <BR>
- * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel) <P>
- *
- * This version is equal to Brian Paul's version 1.2 1999/10/21
- */
+* SimpleJOGL.java <BR>
+* author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel) <P>
+*
+* This version is equal to Brian Paul's version 1.2 1999/10/21
+*/
 public class SimpleJOGL implements GLEventListener {
 
-    //statyczne pola okreœlaj¹ce rotacjê wokó³ osi X i Y
-    private static float xrot = 0.0f, yrot = 0.0f;
+private static float xrot = 0.0f, yrot = 0.0f;
 
-    public static void main(String[] args) {
-        Frame frame = new Frame("Simple JOGL Application");
-        GLCanvas canvas = new GLCanvas();
+public static float ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat³o otaczajšce
+public static float diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };//?wiat³o rozproszone
+public static float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //?wiat³o odbite
+public static float lightPos[] = { 0.0f, 150.0f, 150.0f, 1.0f };//pozycja ?wiat³a
 
-        canvas.addGLEventListener(new SimpleJOGL());
-        frame.add(canvas);
-        frame.setSize(640, 480);
-        final Animator animator = new Animator(canvas);
-        frame.addWindowListener(new WindowAdapter() {
+public static void main(String[] args) {
+Frame frame = new Frame("Simple JOGL Application");
+GLCanvas canvas = new GLCanvas();
 
-            @Override
-            public void windowClosing(WindowEvent e) {
-                // Run this on another thread than the AWT event queue to
-                // make sure the call to Animator.stop() completes before
-                // exiting
-                new Thread(new Runnable() {
+canvas.addGLEventListener(new SimpleJOGL());
+frame.add(canvas);
+frame.setSize(640, 480);
+final Animator animator = new Animator(canvas);
+frame.addWindowListener(new WindowAdapter() {
 
-                    public void run() {
-                        animator.stop();
-                        System.exit(0);
-                    }
-                }).start();
-            }
-        });
-        
-        //Obs³uga klawiszy strza³ek
-    frame.addKeyListener(new KeyListener()
-    {
-        public void keyPressed(KeyEvent e)
-        {
-            if(e.getKeyCode() == KeyEvent.VK_UP)
-            xrot -= 1.0f;
-            if(e.getKeyCode() == KeyEvent.VK_DOWN)
-            xrot +=1.0f;
-            if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-            yrot += 1.0f;
-            if(e.getKeyCode() == KeyEvent.VK_LEFT)
-            yrot -=1.0f;
-        }
-    public void keyReleased(KeyEvent e){}
-    public void keyTyped(KeyEvent e){}
-    });
+@Override
+public void windowClosing(WindowEvent e) {
+// Run this on another thread than the AWT event queue to
+// make sure the call to Animator.stop() completes before
+// exiting
+new Thread(new Runnable() {
 
-        // Center frame
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        animator.start();
-    }
+public void run() {
+animator.stop();
+System.exit(0);
+}
+}).start();
+}
+});
 
-    public void init(GLAutoDrawable drawable) {
-        // Use debug pipeline
-        // drawable.setGL(new DebugGL(drawable.getGL()));
+//Obs³uga klawiszy strza³ek
+frame.addKeyListener(new KeyListener()
+{
+public void keyPressed(KeyEvent e)
+{
+if(e.getKeyCode() == KeyEvent.VK_UP)
+xrot -= 1.0f;
+if(e.getKeyCode() == KeyEvent.VK_DOWN)
+xrot +=1.0f;
+if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+yrot += 1.0f;
+if(e.getKeyCode() == KeyEvent.VK_LEFT)
+yrot -=1.0f;
+if(e.getKeyChar() == 'q')
+    ambientLight = new float[]{ambientLight[0]-0.1f, ambientLight[0]-0.1f, ambientLight[0]-0.1f, ambientLight[0]-0.1f};
+if(e.getKeyChar() == 'w')
+    ambientLight = new float[]{ambientLight[0]+0.1f, ambientLight[0]+0.1f, ambientLight[0]+0.1f, ambientLight[0]+0.1f};
+if(e.getKeyChar() == 'a')
+    diffuseLight = new float[]{diffuseLight[0]-0.1f, diffuseLight[0]-0.1f, diffuseLight[0]-0.1f, diffuseLight[0]-0.1f};
+if(e.getKeyChar() == 's')
+    diffuseLight = new float[]{diffuseLight[0]+0.1f, diffuseLight[0]+0.1f, diffuseLight[0]+0.1f, diffuseLight[0]+0.1f};
+if(e.getKeyChar() == 'z')
+    specular = new float[]{specular[0]-0.1f, specular[0]-0.1f, specular[0]-0.1f, specular[0]-0.1f};
+if(e.getKeyChar() == 'x')
+    specular = new float[]{specular[0]+0.1f, specular[0]+0.1f, specular[0]+0.1f, specular[0]+0.1f};
+if(e.getKeyChar() == 'k')
+    lightPos = new float[]{lightPos[0]-0.1f, lightPos[0]-0.1f, lightPos[0]-0.1f, lightPos[0]-0.1f};
+if(e.getKeyChar() == 'l')
+    lightPos = new float[]{lightPos[0]+0.1f, lightPos[0]+0.1f, lightPos[0]+0.1f, lightPos[0]+0.1f};
 
-        GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
 
-        // Enable VSync
-        gl.setSwapInterval(1);
+}
+public void keyReleased(KeyEvent e){}
+public void keyTyped(KeyEvent e){}
+});
 
-        // Setup the drawing area and shading mode
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
-        
-        //wy³¹czenie wewnêtrzych stron prymitywów
-        gl.glEnable(GL.GL_CULL_FACE);
-
-    }
-
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        GL gl = drawable.getGL();
-        GLU glu = new GLU();
-
-        if (height <= 0) { // avoid a divide by zero error!
-        
-            height = 1;
-        }
-        final float h = (float) width / (float) height;
-        gl.glViewport(0, 0, width, height);
-        gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
-        glu.gluPerspective(45.0f, h, 1.0, 20.0);
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
-    }
-
-    public void display(GLAutoDrawable drawable) {
-        GL gl = drawable.getGL();
-
-        // Clear the drawing area
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-        // Reset the current matrix to the "identity"
-        gl.glLoadIdentity();
-        
-        gl.glTranslatef(0.0f, 0.0f, -6.0f); //przesuniêcie o 6 jednostek
-        gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokó³ osi X
-        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokó³ osi Y
-
-        gl.glBegin(GL.GL_QUADS);
-        //œciana przednia
-        gl.glColor3f(1.0f,0.0f,0.0f);
-        gl.glVertex3f(-1.0f,-1.0f,1.0f);
-        gl.glVertex3f(1.0f,-1.0f,1.0f);
-        gl.glVertex3f(1.0f,1.0f,1.0f);
-        gl.glVertex3f(-1.0f,1.0f,1.0f);
-        //sciana tylnia
-        gl.glColor3f(0.0f,1.0f,0.0f);
-        gl.glVertex3f(-1.0f,1.0f,-1.0f);
-        gl.glVertex3f(1.0f,1.0f,-1.0f);
-        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-        //œciana lewa
-        gl.glColor3f(0.0f,0.0f,1.0f);
-        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-        gl.glVertex3f(-1.0f,-1.0f,1.0f);
-        gl.glVertex3f(-1.0f,1.0f,1.0f);
-        gl.glVertex3f(-1.0f,1.0f,-1.0f);
-        //œciana prawa
-        gl.glColor3f(1.0f,1.0f,0.0f);
-        gl.glVertex3f(1.0f,1.0f,-1.0f);
-        gl.glVertex3f(1.0f,1.0f,1.0f);
-        gl.glVertex3f(1.0f,-1.0f,1.0f);
-        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-        //œciana dolna
-        gl.glColor3f(1.0f,0.0f,1.0f);
-        gl.glVertex3f(-1.0f,-1.0f,1.0f);
-        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-        gl.glVertex3f(1.0f,-1.0f,1.0f);
-        //œciana górna
-        gl.glColor3f(1.5f, 3.3f, 1.0f);
-        gl.glVertex3f(-1.0f,1.0f,1.0f);
-        gl.glVertex3f(1.0f,1.0f,1.0f);
-        gl.glVertex3f(1.0f,1.0f,-1.0f);
-        gl.glVertex3f(-1.0f,1.0f,-1.0f);
-        gl.glEnd();
-        
-
-        // Move the "drawing cursor" around
-        /**gl.glTranslatef(-1.5f, 0.0f, -6.0f);
-        
-        gl.glBegin(GL.GL_TRIANGLES);
-            gl.glColor3f(1.0f,0.0f,0.0f);
-            gl.glVertex3f(-1.0f,4.0f, -6.0f);
-            gl.glVertex3f(-2.0f, 2.0f, -6.0f);
-            gl.glVertex3f(0.0f, 2.0f, -6.0f);
-         gl.glEnd();
-         
-         gl.glTranslatef(-1.0f, 0.0f, -6.0f);
-        
-         gl.glBegin(GL.GL_QUADS);
-            gl.glColor3f(0.0f,8.0f,7.0f);
-            gl.glVertex3f(-1.0f, 2.0f, 0.0f);  // Top Left
-            gl.glVertex3f(1.0f, 2.0f, 0.0f);   // Top Right
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-        gl.glEnd();*/
-        
-      
-
-        // Drawing Using Triangles
-        /**gl.glBegin(GL.GL_TRIANGLES);
-            gl.glColor3f(1.0f, 0.0f, 0.0f);    // Set the current drawing color to red
-            gl.glVertex3f(0.0f, 1.0f, 0.0f);   // Top
-            gl.glColor3f(0.0f, 1.0f, 0.0f);    // Set the current drawing color to green
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-            gl.glColor3f(0.0f, 0.0f, 1.0f);    // Set the current drawing color to blue
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-        // Finished Drawing The Triangle
-        gl.glEnd();
-
-        // Move the "drawing cursor" to another position
-        gl.glTranslatef(3.0f, 0.0f, 0.0f);
-        // Draw A Quad
-        gl.glBegin(GL.GL_QUADS);
-            gl.glColor3f(0.5f, 0.5f, 1.0f);    // Set the current drawing color to light blue
-            gl.glVertex3f(-1.0f, 1.0f, 0.0f);  // Top Left
-            gl.glVertex3f(1.0f, 1.0f, 0.0f);   // Top Right
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-        // Done Drawing The Quad
-        gl.glEnd();*/
-
-        // Flush all drawing operations to the graphics card
-        gl.glFlush();
-    }
-//    public void trojkat(GLAutoDrawable drawable, float x1,float x2,float x3,float y1,float y2,float y3)
-//      {
-//      
-//       GL gl = drawable.getGL();
-//      gl.glTranslatef(-1.5f, 0.0f, -6.0f);
-//        gl.glBegin(GL.GL_TRIANGLES);
-//           
-//            gl.glVertex3f(0.0f, 1.0f, 0.0f);   // Top
-//           
-//            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-//           
-//            gl.glVertex3f(1.0f, -1.0f, 0.0f);  
-//         gl.glEnd();
-//         
-//         
-//      }
-//    
-    
-            
-
-    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
-    }
-    
-    
+// Center frame
+frame.setLocationRelativeTo(null);
+frame.setVisible(true);
+animator.start();
 }
 
+public void init(GLAutoDrawable drawable) {
+// Use debug pipeline
+// drawable.setGL(new DebugGL(drawable.getGL()));
+
+GL gl = drawable.getGL();
+System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+// Enable VSync
+gl.setSwapInterval(1);
+
+// Setup the drawing area and shading mode
+/*gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+gl.glEnable(GL.GL_CULL_FACE);*/
+
+//warto?ci sk³adowe o?wietlenia i koordynaty ?ród³a ?wiat³a
+
+//(czwarty parametr okre?la odleg³o?æ ?ród³a:
+//0.0f-nieskoñczona; 1.0f-okre?lona przez pozosta³e parametry)
+gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
+//ustawienie parametrów ?ród³a ?wiat³a nr. 0
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,0); //swiat³o otaczajšce
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,0); //?wiat³o rozproszone
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //?wiat³o odbite
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja ?wiat³a
+gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród³a ?wiat³a nr. 0
+  gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie œledzenia kolorów
+        //kolory bêd¹ ustalane za pomoc¹ glColor
+        gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
+        //Ustawienie jasnoœci i odblaskowoœci obiektów
+        float specref[] = { 1.0f, 1.0f, 1.0f, 1.0f }; //parametry odblaskowoœci
+        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR,specref,0);
+        
+        gl.glMateriali(GL.GL_FRONT,GL.GL_SHININESS,128);
+
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        // Setup the drawing area and shading mode
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+    }
+
+
+public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+GL gl = drawable.getGL();
+GLU glu = new GLU();
+
+if (height <= 0) { // avoid a divide by zero error!
+
+height = 1;
+}
+final float h = (float) width / (float) height;
+gl.glViewport(0, 0, width, height);
+gl.glMatrixMode(GL.GL_PROJECTION);
+gl.glLoadIdentity();
+glu.gluPerspective(45.0f, h, 1.0, 20.0);
+gl.glMatrixMode(GL.GL_MODELVIEW);
+gl.glLoadIdentity();
+
+
+}
+
+public void display(GLAutoDrawable drawable) {
+GL gl = drawable.getGL();
+
+// Clear the drawing area
+gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+// Reset the current matrix to the "identity"
+gl.glLoadIdentity();
+
+gl.glTranslatef(0.0f, 0.0f, -6.0f); //przesuniêcie o 6 jednostek
+gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f); //rotacja wokó³ osi X
+gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f); //rotacja wokó³ osi Y
+
+gl.glEnable(GL.GL_LIGHTING); //uaktywnienie o?wietlenia
+//ustawienie parametrów ?ród³a ?wiat³a nr. 0
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_AMBIENT,ambientLight,0); //swiat³o otaczajšce
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE,diffuseLight,0); //?wiat³o rozproszone
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //?wiat³o odbite
+gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja ?wiat³a
+gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród³a ?wiat³a nr. 0
+gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie ?ledzenia kolorów
+//kolory bêdš ustalane za pomocš glColor
+// Move the "drawing cursor" around
+//gl.glTranslatef(-1.5f, 1.0f, -6.0f);
+
+// zad.1
+/* gl.glBegin(GL.GL_TRIANGLES);
+gl.glColor3f(0.5f, 0.5f, 0.0f);
+gl.glVertex3f(-1.0f, 1.0f, -6.0f);
+gl.glVertex3f(-2.0f,-1.0f, -6.0f);
+gl.glVertex3f( 0.0f,-1.0f, -6.0f);
+gl.glEnd();
+
+gl.glTranslatef(-1.2f, -2.0f, -6.0f);
+//zad2. a i b
+gl.glBegin(GL.GL_QUADS); 
+gl.glColor3f(1.4f, 0.2f, 0.0f);
+gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+gl.glVertex3f(1.0f, 1.0f, -1.0f); 
+gl.glVertex3f(1.0f, -1.0f, -1.0f);
+gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+
+gl.glEnd(); */
+//ZAD 4.
+/* float x,y,kat;
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glVertex3f(0.0f,0.0f,-6.0f); //œrodek
+for(kat = 0.0f; kat < (2.0f*Math.PI);
+kat+=(Math.PI/32.0f))
+{
+x = 1.5f*(float)Math.sin(kat);
+y = 1.5f*(float)Math.cos(kat);
+gl.glVertex3f(x, y, -1.0f); //kolejne punkty
+}
+gl.glEnd();*/
+
+
+/*gl.glBegin(GL.GL_TRIANGLES);
+gl.glColor3f(1.0f, 0.0f, 0.0f);    // Set the current drawing color to red
+gl.glVertex3f(0.0f, 1.0f, 0.0f);   // Top
+gl.glColor3f(0.0f, 1.0f, 0.0f);    // Set the current drawing color to green
+gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+gl.glColor3f(0.0f, 0.0f, 1.0f);    // Set the current drawing color to blue
+gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
+// Finished Drawing The Triangle
+gl.glEnd();
+
+// Move the "drawing cursor" to another position
+gl.glTranslatef(3.0f, 0.0f, 0.0f);
+// Draw A Quad
+gl.glBegin(GL.GL_QUADS);
+gl.glColor3f(0.5f, 0.5f, 1.0f);    // Set the current drawing color to light blue
+gl.glVertex3f(-1.0f, 1.0f, 0.0f);  // Top Left
+gl.glVertex3f(1.0f, 1.0f, 0.0f);   // Top Right
+gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
+gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+// Done Drawing The Quad
+gl.glEnd();*/
+gl.glBegin(GL.GL_QUADS);
+//œciana przednia
+gl.glColor3f(1.0f,0.0f,0.0f);
+gl.glVertex3f(-1.0f,-1.0f,1.0f);
+gl.glVertex3f(1.0f,-1.0f,1.0f);
+gl.glVertex3f(1.0f,1.0f,1.0f);
+gl.glVertex3f(-1.0f,1.0f,1.0f);
+//sciana tylnia
+gl.glColor3f(0.0f,1.0f,0.0f);
+gl.glVertex3f(-1.0f,1.0f,-1.0f);
+gl.glVertex3f(1.0f,1.0f,-1.0f);
+gl.glVertex3f(1.0f,-1.0f,-1.0f);
+gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+//œciana lewa
+gl.glColor3f(0.0f,0.0f,1.0f);
+gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+gl.glVertex3f(-1.0f,-1.0f,1.0f);
+gl.glVertex3f(-1.0f,1.0f,1.0f);
+gl.glVertex3f(-1.0f,1.0f,-1.0f);
+//œciana prawa
+gl.glColor3f(1.0f,1.0f,0.0f);
+gl.glVertex3f(1.0f,1.0f,-1.0f);
+gl.glVertex3f(1.0f,1.0f,1.0f);
+gl.glVertex3f(1.0f,-1.0f,1.0f);
+gl.glVertex3f(1.0f,-1.0f,-1.0f);
+//œciana dolna
+gl.glColor3f(1.0f,0.0f,1.0f);
+gl.glVertex3f(-1.0f,-1.0f,1.0f);
+gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+gl.glVertex3f(1.0f,-1.0f,-1.0f);
+gl.glVertex3f(1.0f,-1.0f,1.0f);
+//œciana górna
+gl.glColor3f(1.0f,2.0f,1.0f);
+gl.glVertex3f(1.0f,1.0f,-1.0f);
+gl.glVertex3f(-1.0f,1.0f,-1.0f);
+gl.glVertex3f(-1.0f,1.0f,1.0f);
+gl.glVertex3f(1.0f,1.0f,1.0f);
+gl.glEnd();
+
+/*gl.glBegin(GL.GL_TRIANGLES);
+//trojkat1
+gl.glColor3f(1.0f,0.0f,0.0f);
+gl.glVertex3f(-1.0f,-1.0f,0.0f);
+gl.glVertex3f(0.0f,0.0f, 2.0f);
+gl.glVertex3f(-1.0f,1.0f, 0.0f);
+
+
+//trojkat2
+gl.glColor3f(0.0f,1.0f,0.0f);
+gl.glVertex3f(-1.0f,1.0f, 0.0f);
+gl.glVertex3f(0.0f,0.0f, 2.0f);
+gl.glVertex3f(1.0f,1.0f, 0.0f);
+
+//trojkat3
+gl.glColor3f(0.0f,0.0f,1.0f);
+gl.glVertex3f(1.0f,1.0f, 0.0f);
+gl.glVertex3f(0.0f,0.0f, 2.0f);
+gl.glVertex3f(1.0f,-1.0f, 0.0f);
+
+//trojkat4
+gl.glColor3f(1.0f,1.0f,0.0f);
+gl.glVertex3f(1.0f,-1.0f, 0.0f);
+gl.glVertex3f(0.0f,0.0f, 2.0f);
+gl.glVertex3f(-1.0f,-1.0f, 0.0f);
+
+gl.glEnd();
+
+gl.glBegin(GL.GL_QUADS);
+gl.glColor3f(1.0f,0.0f,1.0f);
+gl.glVertex3f(-1.0f,-1.0f,0.0f);
+gl.glVertex3f(-1.0f,1.0f,0.0f);
+gl.glVertex3f(1.0f,1.0f,0.0f);
+gl.glVertex3f(1.0f,-1.0f,0.0f);
+gl.glEnd();*/
+// Flush all drawing operations to the graphics card
+gl.glFlush();
+}
+
+public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+}
+}
