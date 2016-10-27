eb84afb725c6f261fba4a14514f5c1e2c666a49b
student <student@240-06.INFORMATYKA.local>
2016-10-27 12:09:54 +0200

    oswietlenie-ostroslup-proby wektrów

--- src/org/yourorghere/SimpleJOGL.java
+++ src/org/yourorghere/SimpleJOGL.java
@@ -156,6 +156,31 @@ gl.glLoadIdentity();
 
 
 }
+private float[] WyznaczNormalna(float[] punkty, int ind1, int ind2, int ind3)
+{
+ float[] norm = new float[3];
+ float[] wektor0 = new float[3];
+ float[] wektor1 = new float[3];
+
+ for(int i=0;i<3;i++)
+ {
+ wektor0[i]=punkty[i+ind1]-punkty[i+ind2];
+ wektor1[i]=punkty[i+ind2]-punkty[i+ind3];
+ }
+
+ norm[0]=wektor0[1]*wektor1[2]-wektor0[2]*wektor1[1];
+ norm[1]=wektor0[2]*wektor1[0]-wektor0[0]*wektor1[2];
+ norm[2]=wektor0[0]*wektor1[1]-wektor0[1]*wektor1[0];
+ float d=
+(float)Math.sqrt((norm[0]*norm[0])+(norm[1]*norm[1])+ (norm[2]*norm[2]) );
+ if(d==0.0f)
+ d=1.0f;
+ norm[0]/=d;
+ norm[1]/=d;
+ norm[2]/=d;
+
+ return norm;
+}
 
 public void display(GLAutoDrawable drawable) {
 GL gl = drawable.getGL();
@@ -280,19 +305,44 @@ gl.glVertex3f(-1.0f,1.0f,1.0f);
 gl.glVertex3f(1.0f,1.0f,1.0f);
 gl.glEnd();*/
 
-gl.glBegin(GL.GL_TRIANGLES);
+/*gl.glBegin(GL.GL_TRIANGLES);
 //trojkat1
 gl.glColor3f(1.0f,0.0f,0.0f);
+
 gl.glVertex3f(-1.0f,-1.0f,0.0f);
 gl.glVertex3f(0.0f,0.0f, 2.0f);
-gl.glVertex3f(-1.0f,1.0f, 0.0f);
+gl.glVertex3f(-1.0f,1.0f, 0.0f);*/
+
+gl.glBegin(GL.GL_TRIANGLES);
+//œciana przednia
+float[] scianka1={-1.0f,-1.0f,0.0f, //wpó³rzêdne pierwszego punktu
+ 0.0f,0.0f, 2.0f, //wspó³rzêdne drugiego punktu
+ -1.0f,1.0f, 0.0f}; //wspó³rzêdne trzeciego punktu
+float[] normalna1 = WyznaczNormalna(scianka1,0,3,6);
+gl.glNormal3fv(normalna1,0);
+gl.glVertex3fv(scianka1,0); //wspó³rzêdne 1-go punktu zaczynaj¹ siê od indeksu 0
+gl.glVertex3fv(scianka1,3); //wspó³rzêdne 2-go punktu zaczynaj¹ siê od indeksu 3
+gl.glVertex3fv(scianka1,6); //wspó³rzêdne 3-go punktu zaczynaj¹ siê od indeksu 6
+gl.glEnd();
+
 
 
 //trojkat2
-gl.glColor3f(0.0f,1.0f,0.0f);
+/*gl.glColor3f(0.0f,1.0f,0.0f);
 gl.glVertex3f(-1.0f,1.0f, 0.0f);
 gl.glVertex3f(0.0f,0.0f, 2.0f);
-gl.glVertex3f(1.0f,1.0f, 0.0f);
+gl.glVertex3f(1.0f,1.0f, 0.0f);*/
+gl.glBegin(GL.GL_TRIANGLES);
+//œciana przednia
+float[] scianka2={-1.0f,-1.0f,0.0f, //wpó³rzêdne pierwszego punktu
+ 0.0f,0.0f, 2.0f, //wspó³rzêdne drugiego punktu
+ -1.0f,1.0f, 0.0f}; //wspó³rzêdne trzeciego punktu
+float[] normalna2 = WyznaczNormalna(scianka1,0,3,6);
+gl.glNormal3fv(normalna1,0);
+gl.glVertex3fv(scianka1,0); //wspó³rzêdne 1-go punktu zaczynaj¹ siê od indeksu 0
+gl.glVertex3fv(scianka1,3); //wspó³rzêdne 2-go punktu zaczynaj¹ siê od indeksu 3
+gl.glVertex3fv(scianka1,6); //wspó³rzêdne 3-go punktu zaczynaj¹ siê od indeksu 6
+gl.glEnd();
 
 //trojkat3
 gl.glColor3f(0.0f,0.0f,1.0f);
