f0803c6efb23c6f5a64408e5939a33e51e5781bf
student <student@240-05.INFORMATYKA.local>
2016-11-03 10:57:09 +0100

    drzewko bez metody

--- src/org/yourorghere/SimpleJOGL.java
+++ src/org/yourorghere/SimpleJOGL.java
@@ -156,31 +156,102 @@ gl.glLoadIdentity();
 
 
 }
-private float[] WyznaczNormalna(float[] punkty, int ind1, int ind2, int ind3)
-{
- float[] norm = new float[3];
- float[] wektor0 = new float[3];
- float[] wektor1 = new float[3];
-
- for(int i=0;i<3;i++)
+//private float[] WyznaczNormalna(float[] punkty, int ind1, int ind2, int ind3)
+//{
+// float[] norm = new float[3];
+// float[] wektor0 = new float[3];
+// float[] wektor1 = new float[3];
+//
+// for(int i=0;i<3;i++)
+// {
+// wektor0[i]=punkty[i+ind1]-punkty[i+ind2];
+// wektor1[i]=punkty[i+ind2]-punkty[i+ind3];
+// }
+//
+// norm[0]=wektor0[1]*wektor1[2]-wektor0[2]*wektor1[1];
+// norm[1]=wektor0[2]*wektor1[0]-wektor0[0]*wektor1[2];
+// norm[2]=wektor0[0]*wektor1[1]-wektor0[1]*wektor1[0];
+// float d=
+//(float)Math.sqrt((norm[0]*norm[0])+(norm[1]*norm[1])+ (norm[2]*norm[2]) );
+// if(d==0.0f)
+// d=1.0f;
+// norm[0]/=d;
+// norm[1]/=d;
+// norm[2]/=d;
+//
+// return norm;
+//}
+void walec(GL gl)
  {
- wektor0[i]=punkty[i+ind1]-punkty[i+ind2];
- wektor1[i]=punkty[i+ind2]-punkty[i+ind3];
- }
-
- norm[0]=wektor0[1]*wektor1[2]-wektor0[2]*wektor1[1];
- norm[1]=wektor0[2]*wektor1[0]-wektor0[0]*wektor1[2];
- norm[2]=wektor0[0]*wektor1[1]-wektor0[1]*wektor1[0];
- float d=
-(float)Math.sqrt((norm[0]*norm[0])+(norm[1]*norm[1])+ (norm[2]*norm[2]) );
- if(d==0.0f)
- d=1.0f;
- norm[0]/=d;
- norm[1]/=d;
- norm[2]/=d;
-
- return norm;
+//wywo³ujemy automatyczne normalizowanie normalnych
+//bo operacja skalowania je zniekszta³ci
+     
+gl.glEnable(GL.GL_NORMALIZE);
+float x,y,kat;
+gl.glBegin(GL.GL_QUAD_STRIP);
+for(kat = 0.0f; kat < (2.0f*Math.PI); kat += (Math.PI/32.0f))
+{
+x = 0.5f*(float)Math.sin(kat);
+y = 0.5f*(float)Math.cos(kat);
+gl.glNormal3f((float)Math.sin(kat),(float)Math.cos(kat),0.0f);
+gl.glVertex3f(x, y, -1.0f);
+gl.glVertex3f(x, y, 0.0f);
 }
+gl.glEnd();
+gl.glNormal3f(0.0f,0.0f,-1.0f);
+x=y=kat=0.0f;
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glVertex3f(0.0f, 0.0f, -1.0f); //srodek kola
+for(kat = 0.0f; kat < (2.0f*Math.PI); kat += (Math.PI/32.0f))
+{
+x = 0.5f*(float)Math.sin(kat);
+y = 0.5f*(float)Math.cos(kat);
+gl.glVertex3f(x, y, -1.0f);
+}
+gl.glEnd();
+gl.glNormal3f(0.0f,0.0f,1.0f);
+x=y=kat=0.0f;
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glVertex3f(0.0f, 0.0f, 0.0f); //srodek kola
+for(kat = 2.0f*(float)Math.PI; kat > 0.0f ; kat -= (Math.PI/32.0f))
+{
+x = 0.5f*(float)Math.sin(kat);
+y = 0.5f*(float)Math.cos(kat);
+gl.glVertex3f(x, y, 0.0f);
+}
+gl.glEnd();
+}
+
+void stozek(GL gl)
+{
+//wywo³ujemy automatyczne normalizowanie normalnych
+    
+gl.glEnable(GL.GL_NORMALIZE);
+float x,y,kat;
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glVertex3f(0.0f, 0.0f, -2.0f); //wierzcholek stozka
+for(kat = 0.0f; kat < (2.0f*Math.PI); kat += (Math.PI/32.0f))
+{
+x = (float)Math.sin(kat);
+y = (float)Math.cos(kat);
+gl.glNormal3f((float)Math.sin(kat),(float)Math.cos(kat),-2.0f);
+gl.glVertex3f(x, y, 0.0f);
+}
+gl.glEnd();
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glNormal3f(0.0f,0.0f,1.0f);
+gl.glVertex3f(0.0f, 0.0f, 0.0f); //srodek kola
+for(kat = 2.0f*(float)Math.PI; kat > 0.0f; kat -= (Math.PI/32.0f))
+{
+x = (float)Math.sin(kat);
+y = (float)Math.cos(kat);
+gl.glVertex3f(x, y, 0.0f);
+}
+gl.glEnd();
+}
+
+
+
 
 public void display(GLAutoDrawable drawable) {
 GL gl = drawable.getGL();
@@ -202,6 +273,26 @@ gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR,specular,0); //?wiat³o odbite
 gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION,lightPos,0); //pozycja ?wiat³a
 gl.glEnable(GL.GL_LIGHT0); //uaktywnienie ?ród³a ?wiat³a nr. 0
 gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie ?ledzenia kolorów
+
+
+gl.glColor3f(0.0f, 0.0f, 0.0f);
+walec(gl);
+gl.glTranslatef(0.0f, 0.0f, -1.0f);
+
+
+
+gl.glColor3f(0.5f, 0.5f, 0.0f);
+stozek(gl);
+
+gl.glTranslatef(0.0f, 0.0f, -1.0f);
+gl.glScalef(0.7f, 0.7f, 0.7f);
+stozek(gl);
+
+gl.glTranslatef(0.0f, 0.0f, -1.0f);
+gl.glScalef(0.9f, 0.9f, 0.9f);
+stozek(gl);
+
+
 //kolory bêdš ustalane za pomocš glColor
 // Move the "drawing cursor" around
 //gl.glTranslatef(-1.5f, 1.0f, -6.0f);
@@ -313,17 +404,17 @@ gl.glVertex3f(-1.0f,-1.0f,0.0f);
 gl.glVertex3f(0.0f,0.0f, 2.0f);
 gl.glVertex3f(-1.0f,1.0f, 0.0f);*/
 
-gl.glBegin(GL.GL_TRIANGLES);
-//œciana przednia
-float[] scianka1={-1.0f,-1.0f,0.0f, //wpó³rzêdne pierwszego punktu
- 0.0f,0.0f, 2.0f, //wspó³rzêdne drugiego punktu
- -1.0f,1.0f, 0.0f}; //wspó³rzêdne trzeciego punktu
-float[] normalna1 = WyznaczNormalna(scianka1,0,3,6);
-gl.glNormal3fv(normalna1,0);
-gl.glVertex3fv(scianka1,0); //wspó³rzêdne 1-go punktu zaczynaj¹ siê od indeksu 0
-gl.glVertex3fv(scianka1,3); //wspó³rzêdne 2-go punktu zaczynaj¹ siê od indeksu 3
-gl.glVertex3fv(scianka1,6); //wspó³rzêdne 3-go punktu zaczynaj¹ siê od indeksu 6
-gl.glEnd();
+//gl.glBegin(GL.GL_TRIANGLES);
+////œciana przednia
+//float[] scianka1={-1.0f,-1.0f,0.0f, //wpó³rzêdne pierwszego punktu
+// 0.0f,0.0f, 2.0f, //wspó³rzêdne drugiego punktu
+// -1.0f,1.0f, 0.0f}; //wspó³rzêdne trzeciego punktu
+//float[] normalna1 = WyznaczNormalna(scianka1,0,3,6);
+//gl.glNormal3fv(normalna1,0);
+//gl.glVertex3fv(scianka1,0); //wspó³rzêdne 1-go punktu zaczynaj¹ siê od indeksu 0
+//gl.glVertex3fv(scianka1,3); //wspó³rzêdne 2-go punktu zaczynaj¹ siê od indeksu 3
+//gl.glVertex3fv(scianka1,6); //wspó³rzêdne 3-go punktu zaczynaj¹ siê od indeksu 6
+//gl.glEnd();
 
 
 
@@ -332,39 +423,39 @@ gl.glEnd();
 gl.glVertex3f(-1.0f,1.0f, 0.0f);
 gl.glVertex3f(0.0f,0.0f, 2.0f);
 gl.glVertex3f(1.0f,1.0f, 0.0f);*/
-gl.glBegin(GL.GL_TRIANGLES);
-//œciana przednia
-float[] scianka2={-1.0f,-1.0f,0.0f, //wpó³rzêdne pierwszego punktu
- 0.0f,0.0f, 2.0f, //wspó³rzêdne drugiego punktu
- -1.0f,1.0f, 0.0f}; //wspó³rzêdne trzeciego punktu
-float[] normalna2 = WyznaczNormalna(scianka1,0,3,6);
-gl.glNormal3fv(normalna1,0);
-gl.glVertex3fv(scianka1,0); //wspó³rzêdne 1-go punktu zaczynaj¹ siê od indeksu 0
-gl.glVertex3fv(scianka1,3); //wspó³rzêdne 2-go punktu zaczynaj¹ siê od indeksu 3
-gl.glVertex3fv(scianka1,6); //wspó³rzêdne 3-go punktu zaczynaj¹ siê od indeksu 6
-gl.glEnd();
+//gl.glBegin(GL.GL_TRIANGLES);
+////œciana przednia
+//float[] scianka2={-1.0f,-1.0f,0.0f, //wpó³rzêdne pierwszego punktu
+// 0.0f,0.0f, 2.0f, //wspó³rzêdne drugiego punktu
+// -1.0f,1.0f, 0.0f}; //wspó³rzêdne trzeciego punktu
+//float[] normalna2 = WyznaczNormalna(scianka1,0,3,6);
+//gl.glNormal3fv(normalna1,0);
+//gl.glVertex3fv(scianka1,0); //wspó³rzêdne 1-go punktu zaczynaj¹ siê od indeksu 0
+//gl.glVertex3fv(scianka1,3); //wspó³rzêdne 2-go punktu zaczynaj¹ siê od indeksu 3
+//gl.glVertex3fv(scianka1,6); //wspó³rzêdne 3-go punktu zaczynaj¹ siê od indeksu 6
+//gl.glEnd();
 
-//trojkat3
-gl.glColor3f(0.0f,0.0f,1.0f);
-gl.glVertex3f(1.0f,1.0f, 0.0f);
-gl.glVertex3f(0.0f,0.0f, 2.0f);
-gl.glVertex3f(1.0f,-1.0f, 0.0f);
-
-//trojkat4
-gl.glColor3f(1.0f,1.0f,0.0f);
-gl.glVertex3f(1.0f,-1.0f, 0.0f);
-gl.glVertex3f(0.0f,0.0f, 2.0f);
-gl.glVertex3f(-1.0f,-1.0f, 0.0f);
-
-gl.glEnd();
-
-gl.glBegin(GL.GL_QUADS);
-gl.glColor3f(1.0f,0.0f,1.0f);
-gl.glVertex3f(-1.0f,-1.0f,0.0f);
-gl.glVertex3f(-1.0f,1.0f,0.0f);
-gl.glVertex3f(1.0f,1.0f,0.0f);
-gl.glVertex3f(1.0f,-1.0f,0.0f);
-gl.glEnd();
+////trojkat3
+//gl.glColor3f(0.0f,0.0f,1.0f);
+//gl.glVertex3f(1.0f,1.0f, 0.0f);
+//gl.glVertex3f(0.0f,0.0f, 2.0f);
+//gl.glVertex3f(1.0f,-1.0f, 0.0f);
+//
+////trojkat4
+//gl.glColor3f(1.0f,1.0f,0.0f);
+//gl.glVertex3f(1.0f,-1.0f, 0.0f);
+//gl.glVertex3f(0.0f,0.0f, 2.0f);
+//gl.glVertex3f(-1.0f,-1.0f, 0.0f);
+//
+//gl.glEnd();
+//
+//gl.glBegin(GL.GL_QUADS);
+//gl.glColor3f(1.0f,0.0f,1.0f);
+//gl.glVertex3f(-1.0f,-1.0f,0.0f);
+//gl.glVertex3f(-1.0f,1.0f,0.0f);
+//gl.glVertex3f(1.0f,1.0f,0.0f);
+//gl.glVertex3f(1.0f,-1.0f,0.0f);
+//gl.glEnd();
 // Flush all drawing operations to the graphics card
 gl.glFlush();
 }
